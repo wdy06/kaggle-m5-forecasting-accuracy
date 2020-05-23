72e232fa6b0edbb42967ccebae211802a78e5879
wdy06 <mituhashitakashi@gmail.com>
2020-05-23 13:42:52 +0000

    bug fix in generating feature

--- src/features.py
+++ src/features.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import sandesh
+from tqdm import tqdm
 
 import preprocessing
 import utils
@@ -8,12 +10,40 @@ from dataset import M5Dataset
 
 def simple_feature(data):
 
+    group_ids = ('all_id', 'state_id', 'store_id', 'cat_id', 'dept_id', 'item_id',
+                 ['state_id', 'cat_id'],  ['state_id',
+                                           'dept_id'], ['store_id', 'cat_id'],
+                 ['store_id', 'dept_id'], ['item_id', 'state_id'], ['item_id', 'store_id'])
+    group_id_names = ('all', 'state', 'store', 'cat', 'dept', 'item',
+                      'state_cat', 'state_dept', 'store_cat',
+                      'store_dept', 'item_state', 'item_store')
+    data['all_id'] = 'all'
     # rolling demand features
     # lag feature
     # lag_list = [28, 29, 30]
-    for lag in [28, 29, 30]:
+    for lag in tqdm([28, 29, 30]):
         data[f'lag_t{lag}'] = data.groupby(
             ['id'])['demand'].transform(lambda x: x.shift(lag))
+        # for group_id, id_name in zip(group_ids, group_id_names):
+        #     data[f'lag_t{lag}_{id_name}'] = data.groupby(
+        #         group_id)['demand'].transform(lambda x: x.shift(lag))
+    for lag in tqdm([28, 29, 30]):
+        print(lag)
+        for group_id, id_name in zip(group_ids, group_id_names):
+            # data[f'lag_t28_{id_name}'] = data.groupby(
+            #     group_id)['demand'].transform(lambda x: x.shift(lag))
+            if isinstance(group_id, str):
+                group_id = [group_id]
+            # data[f'lag_t28_{id_name}'] = data.groupby(
+            #     group_id + ['date'])['lag_t28'].transform(np.mean)
+            tmp = data.groupby(
+                group_id + ['date'])['demand'].mean().groupby(group_id).apply(lambda x: x.shift(lag))
+            tmp.name = f'lag_t{lag}_{id_name}'
+            tmp = tmp.reset_index()
+            data = pd.merge(data, tmp, on=group_id + ['date'])
+
+            # data[f'lag_t{lag}_{id_name}'] = data.groupby(
+            #     group_id + ['date'])['demand'].mean().groupby(group_id).transform(lambda x: x.shift(lag)).values
 
     # rolling mean
     # rolling_list = [7, 14, 21, 28]
@@ -22,16 +52,17 @@ def simple_feature(data):
     #         ['id'])['demand'].transform(lambda x: x.rolling(window_size).mean())
 
     # lag rolling
-    for lag in [28]:
-        for window in [7, 30, 90, 180]:
-            data[f'lag{lag}_rolling_mean_t{window}'] = data.groupby(
-                ['id'])['demand'].transform(lambda x: x.shift(lag).rolling(window).mean())
-            data[f'lag{lag}_rolling_std_t{window}'] = data.groupby(
-                ['id'])['demand'].transform(lambda x: x.shift(lag).rolling(window).std())
-            data[f'lag{lag}_rolling_skew_t{window}'] = data.groupby(
-                ['id'])['demand'].transform(lambda x: x.shift(lag).rolling(window).skew())
-            data[f'lag{lag}_rolling_kurt_t{window}'] = data.groupby(
-                ['id'])['demand'].transform(lambda x: x.shift(lag).rolling(window).kurt())
+    # for lag in [28]:
+    #     for window in [7, 30, 90, 180]:
+    #         for group_id, id_name in zip(group_ids, group_id_names):
+    #             data[f'lag{lag}_rolling_mean_t{window}_{id_name}'] = data.groupby(
+    #                 group_id)['demand'].transform(lambda x: x.shift(lag).rolling(window).mean())
+    #             data[f'lag{lag}_rolling_std_t{window}_{id_name}'] = data.groupby(
+    #                 group_id)['demand'].transform(lambda x: x.shift(lag).rolling(window).std())
+    #             data[f'lag{lag}_rolling_skew_t{window}_{id_name}'] = data.groupby(
+    #                 group_id)['demand'].transform(lambda x: x.shift(lag).rolling(window).skew())
+    #             data[f'lag{lag}_rolling_kurt_t{window}_{id_name}'] = data.groupby(
+    #                 group_id)['demand'].transform(lambda x: x.shift(lag).rolling(window).kurt())
 
     # price features
     data['lag_price_t1'] = data.groupby(
@@ -66,17 +97,27 @@ def generate_features(data):
 if __name__ == '__main__':
     output_path = utils.FEATURE_DIR / 'baseline_features.pkl'
     encoder_path = utils.FEATURE_DIR / 'encoder.pkl'
-    print('generating features...')
-    dataset = M5Dataset()
-    data = preprocessing.melt_and_merge(
-        dataset.calendar, dataset.sell_prices, dataset.main_df, dataset.submission,
-        merge=True)
-    # label encoding
-    cat = ['item_id', 'dept_id', 'cat_id', 'store_id', 'state_id',
-           'event_name_1', 'event_type_1', 'event_name_2', 'event_type_2']
-    data, encoder = preprocessing.label_encoding(df=data, cat_features=cat,
-                                                 verbose=True)
-    data = generate_features(data)
-    utils.dump_pickle(data, output_path)
-    utils.dump_pickle(encoder, encoder_path)
-    print('finished generating features !!')
+    melted_path = utils.FEATURE_DIR / 'melted.pkl'
+    sandesh.send(f'start generating feature')
+    try:
+        print('generating features...')
+        # dataset = M5Dataset()
+        # data = preprocessing.melt_and_merge(
+        #     dataset.calendar, dataset.sell_prices, dataset.main_df, dataset.submission,
+        #     merge=True)
+        # utils.dump_pickle(data, melted_path)
+        data = utils.load_pickle(melted_path)
+        # label encoding
+        cat = ['item_id', 'dept_id', 'cat_id', 'store_id', 'state_id',
+               'event_name_1', 'event_type_1', 'event_name_2', 'event_type_2']
+        data, encoder = preprocessing.label_encoding(df=data, cat_features=cat,
+                                                     verbose=True)
+        data = generate_features(data)
+        utils.dump_pickle(data, output_path)
+        utils.dump_pickle(encoder, encoder_path)
+        print('finished generating features !!')
+    except Exception as e:
+        print(e)
+        sandesh.send(str(e))
+    else:
+        sandesh.send(f'finished generating features !!')
